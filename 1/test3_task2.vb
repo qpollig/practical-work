d4cfa906a3d672bda026970daa5dc70d4a897e64
Polina Nemechkina <136108608+qpollig@users.noreply.github.com>
2025-01-18 04:06:35 +0500

    Create test3_task2.vb

---
+++ 1/test3_task2.vb
@@ -0,0 +1,26 @@
+Module Module1
+    Sub Main()
+        ' Объявление переменной
+        Dim x As Double
+        Dim y As Double
+
+        ' Ввод значения переменной x
+        Console.Write("Введите значение x: ")
+        x = Convert.ToDouble(Console.ReadLine())
+
+        ' Определение значения y в зависимости от x
+        If x < -1 Then
+            y = Math.Sin(x + 1) ^ 2
+        ElseIf x >= -1 AndAlso x < 1 Then
+            y = -(1 + Math.Cos(Math.PI * x))
+        Else
+            y = Math.Sin(x - 1) ^ 2
+        End If
+
+        ' Вывод результата
+        Console.WriteLine("Результат y: " & y)
+
+        ' Ожидание нажатия клавиши для завершения программы
+        Console.ReadKey()
+    End Sub
+End Module
